2631ff28778ee4a203a08264644218c9ceab4294
DWesl <22566757+DWesl@users.noreply.github.com>
2022-05-02 20:37:51 -0400

    FIX: Revert earlier debug change.

--- .ci/install.sh
+++ .ci/install.sh
@@ -25,7 +25,6 @@ if [[ $(uname) != CYGWIN* ]]; then
                              cmake meson imagemagick libharfbuzz-dev libfribidi-dev
 fi
 
-uname -mo
 if [[ $(uname -mo) != i*86" Cygwin" ]]; then
     python3 -m pip install --upgrade pip
 fi
